86d49e4b35282018eeebca6251d67dba1e7cee70
James Budge <thebudgie@gmail.com>
2019-04-16 15:41:19 +0100

    completed all lab tasks

--- console.rb
+++ console.rb
@@ -1,9 +1,26 @@
 require_relative('models/property.rb')
 
+#Property.delete_all()
+
 house1 = { 'address' => '1 High Street', 'value' => '10000', 'number_of_bedrooms' => 4, 'buylet_status' => 'Buy'}
 house2 = { 'address' => '20 King Place', 'value' => '2000000', 'number_of_bedrooms' => 10, 'buylet_status' => 'Buy'}
 
 query1 = Property.new(house1)
 query2 = Property.new(house2)
 
-query1.save()
+# query1.save()
+# query1.save()
+# query1.save()
+# query1.save()
+# query1.save()
+# query2.save()
+# query2.delete()
+query1.buylet_status = "Let"
+query1.update()
+
+p Property.find('108')
+p Property.find_by_address('20 King Place')
+p Property.find_by_address('My House')
+
+
+# p Property.all()
--- models/property.rb
+++ models/property.rb
@@ -1,4 +1,6 @@
 require('pg')
+require('pry-byebug')
+
 
 class Property
 
@@ -6,27 +8,87 @@ class Property
   attr_reader :id
 
 def initialize( options )
+  @id = options['id'].to_i if options['id']
   @address = options['address']
   @number_of_bedrooms = options['number_of_bedrooms'].to_i
   @value = options['value'].to_i
   @buylet_status = options['buylet_status']
-  @id = options['id'].to_i if options['id']
 end
 
 def save()
   db = PG.connect({dbname: 'property_tracker', host: 'localhost'});
-  sql = "INSERT INTO property_tracker (address, value, number_of_bedrooms, buylet_status) VALUES ($1, $2, $3, $4)"
+  sql = "INSERT INTO property_tracker (address, value, number_of_bedrooms, buylet_status) VALUES ($1, $2, $3, $4) RETURNING id;"
   values = [@address, @value, @number_of_bedrooms, @buylet_status]
   db.prepare("save", sql)
-  db.exec_prepared("save", values)
-
-
+  property_hashes = db.exec_prepared("save", values)
+  @id = property_hashes[0]['id']
   db.close()
 end
 
+def delete()
+  db = PG.connect({dbname: 'property_tracker', host: 'localhost'})
+  sql = "DELETE FROM property_tracker WHERE id = $1;"
+  values = [@id]
+  db.prepare("delete_one", sql)
+  db.exec_prepared("delete_one", values)
+  db.close()
+end
 
+def Property.delete_all()
+  db = PG.connect({dbname: 'property_tracker', host: 'localhost'});
+  sql = "DELETE FROM property_tracker;"
+  db.prepare("delete_all", sql)
+  db.exec_prepared("delete_all")
+  db.close()
+end
 
+def Property.all()
+  db = PG.connect({dbname: 'property_tracker', host: 'localhost'});
+  sql = "SELECT * FROM property_tracker;"
+  property_hashes = db.exec(sql)
+  property_object = property_hashes.map { |property| Property.new(property) }
+  db.close()
+  return property_object
+end
 
+def update
+  db = PG.connect({dbname: 'property_tracker', host: 'localhost'});
+  sql = "UPDATE property_tracker SET (address, value, number_of_bedrooms, buylet_status) = ($1,$2,$3,$4) WHERE id = $5"
+  values = [@address,@value,@number_of_bedrooms,@buylet_status,@id]
+  db.prepare("update", sql)
+  db.exec_prepared("update", values)
+  db.close()
+end
 
+def Property.find(id)
+  db = PG.connect({dbname: 'property_tracker', host: 'localhost'});
+  sql = "SELECT * FROM property_tracker WHERE id = $1"
+  values = [id]
+  db.prepare("find", sql)
+  prep = db.exec_prepared("find", values)
+  # binding.pry
+  #property_hashes = db.exec(prep)
+  #property_object = property_hashes.map { | property| Property.new(property)}
+  db.close()
+  return prep[0]
+end
+
+def Property.find_by_address(address)
+  db = PG.connect({dbname: 'property_tracker', host: 'localhost'});
+  sql = "SELECT * FROM property_tracker WHERE address = $1"
+  values = [address]
+  db.prepare("find", sql)
+  prep = db.exec_prepared("find", values)
+  # p prep
+  db.close()
+  if prep.ntuples == 0
+    return nil
+  else
+
+  # binding.pry
+  return prep[0] unless prep[0].nil?
+  end
+
+end
 
 end
